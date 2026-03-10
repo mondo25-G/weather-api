c92bbef7ca35bd980d6a8e64714ab6aacd48e7e0
George Chatzidakis <g.chatzidakis@outlook.com.gr>
2026-03-10 11:38:23 +0200

    enable caching

---
+++ src/main/java/com/api/weather/configuration/RedisCacheConfig.java
@@ -0,0 +1,28 @@
+package com.api.weather.configuration;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues();
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfig)
+                .build();
+    }
+}
--- src/main/java/com/api/weather/entity/WeatherRecord.java
+++ src/main/java/com/api/weather/entity/WeatherRecord.java
@@ -4,9 +4,11 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.Serializable;
+
 @Data
 @Document(collection = "weather_records")
-public class WeatherRecord {
+public class WeatherRecord implements Serializable {
 
     @Id
     private String id;
