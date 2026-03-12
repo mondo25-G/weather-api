79a90460fafaa8c22daf62c460024557d570047b
George Chatzidakis <g.chatzidakis@outlook.com.gr>
2026-03-12 13:01:03 +0200

    request mapping fix

--- src/main/java/com/api/weather/controller/WeatherController.java
+++ src/main/java/com/api/weather/controller/WeatherController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/com/api/weather")
+@RequestMapping("/weather")
 @RequiredArgsConstructor
 public class WeatherController {
 
